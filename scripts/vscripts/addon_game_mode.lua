b1921fd1ebbd6172c5242c4a6233b6f6f6edc325
Hertin <hertin@outlook.com>
2016-02-23 23:17:36 -0600

    Added files via upload
    upload my local work

---
+++ scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,28 @@
+-- Generated from template
+require('gamemode')
+
+function Precache( context )
+			PrecacheResource( "soundfile", "soundevents/game_sounds_heroes/game_sounds_morphling.vsndevts", context )
+			PrecacheResource( "particle", "particles/units/heroes/hero_morphling/morphling_waveform.vpcf", context )
+			PrecacheResource( "particle", "particles/units/heroes/hero_morphling/morphling_base_attack.vpcf", context )
+			PrecacheResource( "particle", "particles/units/heroes/hero_morphling/morphling_base_attack_trail.vpcf", context)
+			PrecacheResource( "soundfile", "soundevents/game_sounds_heroes/game_sounds_morphling.vsndevts", context )
+			PrecacheResource( "particle", "particles/econ/items/mirana/mirana_crescent_arrow/mirana_spell_crescent_arrow.vpcf", context)
+			PrecacheResource( "particle", "particles/econ/items/morphling/morphling_ethereal/morphling_adaptive_strike_ethereal.vpcf", context)
+			PrecacheResource( "particle", "particles/morphling_adaptive_strike_ethereal.vpcf", context)
+			PrecacheResource( "particle", "particles/morphling_base_attack.vpcf", context)
+			PrecacheResource( "particle", "particles/units/heroes/hero_lina/lina_spell_light_strike_array.vpcf", context)	
+			PrecacheResource( "particle", "particles/morphling_adaptive_strike_ethereal2.vpcf", context)	
+			PrecacheResource( "particle", "particles/waveform.vpcf", context )		
+			PrecacheResource( "particle", "particles/units/heroes/hero_chaos_knight/chaos_knight_phantasm.vpcf", context )	
+			PrecacheResource( "particle", "particles/units/heroes/hero_puck/puck_phase_shift.vpcf", context )	
+			PrecacheResource( "particle", "particles/units/heroes/hero_morphling/morphling_ambient_new.vpcf", context )	
+			PrecacheResource( "soundfile", "soundevents/game_sounds_heroes/game_sounds_mirana.vsndevts", context)
+			PrecacheResource( "particle", "particles/econ/items/puck/puck_alliance_set/puck_illusory_orb_aproset.vpcf", context )
+end
+
+-- Create the game mode when we activate
+function Activate()
+  GameRules.GameMode = GameMode()
+  GameRules.GameMode:InitGameMode()
+end
---
+++ scripts/vscripts/gamemode.lua
@@ -0,0 +1,265 @@
+-- This is the primary barebones gamemode script and should be used to assist in initializing your game mode
+
+have_set_respawn_vector=false
+respawn_vector={}
+
+if GameMode == nil then
+  GameMode = class({})
+end
+
+-- This library allow for easily delayed/timed actions
+require('libraries/timers')
+-- This library can be used for advancted physics/motion/collision of units.  See PhysicsReadme.txt for more information.
+require('libraries/physics')
+-- This library can be used for advanced 3D projectile systems.
+require('libraries/projectiles')
+-- This library can be used for sending panorama notifications to the UIs of players/teams/everyone
+require('libraries/notifications')
+-- This library can be used for starting customized animations on units from lua
+require('libraries/animations')
+-- This library can be used for performing "Frankenstein" attachments on units
+require('libraries/attachments')
+
+
+-- settings.lua is where you can specify many different properties for your game mode and is one of the core barebones files.
+require('settings')
+-- events.lua is where you can specify the actions to be taken when any event occurs and is one of the core barebones files.
+require('events')
+
+--[[
+  This function should be used to set up Async precache calls at the beginning of the gameplay.
+
+  In this function, place all of your PrecacheItemByNameAsync and PrecacheUnitByNameAsync.  These calls will be made
+  after all players have loaded in, but before they have selected their heroes. PrecacheItemByNameAsync can also
+  be used to precache dynamically-added datadriven abilities instead of items.  PrecacheUnitByNameAsync will 
+  precache the precache{} block statement of the unit and all precache{} block statements for every Ability# 
+  defined on the unit.
+
+  This function should only be called once.  If you want to/need to precache more items/abilities/units at a later
+  time, you can call the functions individually (for example if you want to precache units in a new wave of
+  holdout).
+
+  This function should generally only be used if the Precache() function in addon_game_mode.lua is not working.
+]]
+function GameMode:PostLoadPrecache()
+  
+  --PrecacheItemByNameAsync("item_example_item", function(...) end)
+  --PrecacheItemByNameAsync("example_ability", function(...) end)
+
+  --PrecacheUnitByNameAsync("npc_dota_hero_viper", function(...) end)
+  --PrecacheUnitByNameAsync("npc_dota_hero_enigma", function(...) end)
+end
+
+--[[
+  This function is called once and only once as soon as the first player (almost certain to be the server in local lobbies) loads in.
+  It can be used to initialize state that isn't initializeable in InitGameMode() but needs to be done before everyone loads in.
+]]
+function GameMode:OnFirstPlayerLoaded()
+
+end
+
+--[[
+  This function is called once and only once after all players have loaded into the game, right as the hero selection time begins.
+  It can be used to initialize non-hero player state or adjust the hero selection (i.e. force random etc)
+]]
+function GameMode:OnAllPlayersLoaded()
+
+end
+
+--[[
+  This function is called once and only once for every player when they spawn into the game for the first time.  It is also called
+  if the player's hero is replaced with a new hero for any reason.  This function is useful for initializing heroes, such as adding
+  levels, changing the starting gold, removing/adding abilities, adding physics, etc.
+
+  The hero parameter is the hero entity that just spawned in
+]]
+
+
+  --[[ --These lines if uncommented will replace the W ability of any hero that loads into the game
+    --with the "example_ability" ability
+
+  local abil = hero:GetAbilityByIndex(1)
+  hero:RemoveAbility(abil:GetAbilityName())
+  hero:AddAbility("example_ability")]]
+
+
+--[[
+  This function is called once and only once when the game completely begins (about 0:00 on the clock).  At this point,
+  gold will begin to go up in ticks if configured, creeps will spawn, towers will become damageable etc.  This function
+  is useful for starting any game logic timers/thinkers, beginning the first round, etc.
+]]
+function GameMode:OnGameInProgress()
+  Timers:CreateTimer(30, -- Start this timer 30 game-time seconds later
+    function()
+      return 30.0 -- Rerun this timer every 30 game-time seconds 
+    end)
+end
+
+
+
+
+-- This function initializes the game mode and is called before anyone loads into the game
+-- It can be used to pre-initialize any values/tables that will be needed later
+function GameMode:InitGameMode()
+  GameMode = self
+
+  if not have_set_respawn_vector then
+    have_set_respawn_vector = true
+    local mapname = GetMapName()
+    local mx=1900
+    local my=1900
+    local ct=128
+    for x= -mx,mx,200 do
+      for y = -my,my,200 do
+        local p = Vector(x,y,128)
+        if GridNav:CanFindPath(p, Vector(0, 0, ct)) then
+          table.insert(respawn_vector,p)
+        end
+      end
+    end
+  end
+
+
+  -- Call the internal function to set up the rules/behaviors specified in constants.lua
+  -- This also sets up event hooks for all event handlers in events.lua
+  -- Check out internals/gamemode to see/modify the exact code
+  GameMode:_InitGameMode()
+
+  -- Commands can be registered for debugging purposes or as functions that can be called by the custom Scaleform UI
+  Convars:RegisterCommand( "command_example", Dynamic_Wrap(GameMode, 'ExampleConsoleCommand'), "A console command example", FCVAR_CHEAT )
+
+end
+
+
+
+function GameMode:_InitGameMode()
+  -- Setup rules
+  GameRules:SetHeroRespawnEnabled( ENABLE_HERO_RESPAWN )
+  GameRules:SetUseUniversalShopMode( UNIVERSAL_SHOP_MODE )
+  GameRules:SetSameHeroSelectionEnabled( ALLOW_SAME_HERO_SELECTION )
+  GameRules:SetHeroSelectionTime( HERO_SELECTION_TIME )
+  GameRules:SetPreGameTime( PRE_GAME_TIME)
+  GameRules:SetPostGameTime( POST_GAME_TIME )
+  GameRules:SetTreeRegrowTime( TREE_REGROW_TIME )
+  GameRules:SetUseCustomHeroXPValues ( USE_CUSTOM_XP_VALUES )
+  GameRules:SetGoldPerTick(GOLD_PER_TICK)
+  GameRules:SetGoldTickTime(GOLD_TICK_TIME)
+  GameRules:SetRuneSpawnTime(RUNE_SPAWN_TIME)
+  GameRules:SetUseBaseGoldBountyOnHeroes(USE_STANDARD_HERO_GOLD_BOUNTY)
+  GameRules:SetHeroMinimapIconScale( MINIMAP_ICON_SIZE )
+  GameRules:SetCreepMinimapIconScale( MINIMAP_CREEP_ICON_SIZE )
+  GameRules:SetRuneMinimapIconScale( MINIMAP_RUNE_ICON_SIZE )
+
+  GameRules:SetFirstBloodActive( ENABLE_FIRST_BLOOD )
+  GameRules:SetHideKillMessageHeaders( HIDE_KILL_BANNERS )
+  -- This is multiteam configuration stuff
+    -- This is multiteam configuration stuff
+
+      GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_GOODGUYS, 10 )
+      GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_BADGUYS, 10 )--每队人数
+
+  -- if USE_CUSTOM_TEAM_COLORS then
+  --   for team,color in pairs(TEAM_COLORS) do
+  --     SetTeamCustomHealthbarColor(team, color[1], color[2], color[3])
+  --   end
+  -- end
+
+  --InitLogFile( "log/barebones.txt","")
+
+  -- Event Hooks
+  -- All of these events can potentially be fired by the game, though only the uncommented ones have had
+  -- Functions supplied for them.  If you are interested in the other events, you can uncomment the
+  -- ListenToGameEvent line and add a function to handle the event
+
+  ListenToGameEvent('entity_killed', Dynamic_Wrap(GameMode, 'OnEntityKilled'), self)
+  --ListenToGameEvent('player_connect_full', Dynamic_Wrap(GameMode, 'OnConnectFull'), self)
+  --ListenToGameEvent('player_disconnect', Dynamic_Wrap(GameMode, 'OnDisconnect'), self)
+
+
+  ListenToGameEvent('player_connect', Dynamic_Wrap(GameMode, 'PlayerConnect'), self)
+  --ListenToGameEvent('game_rules_state_change', Dynamic_Wrap(GameMode, 'OnGameRulesStateChange'), self)
+  ListenToGameEvent('npc_spawned', Dynamic_Wrap(GameMode, 'OnNPCSpawned'), self)
+  ListenToGameEvent('dota_player_pick_hero', Dynamic_Wrap(GameMode, 'OnPlayerPickHero'), self)
+  ListenToGameEvent('dota_team_kill_credit', Dynamic_Wrap(GameMode, 'OnTeamKillCredit'), self)
+  --ListenToGameEvent("player_reconnected", Dynamic_Wrap(GameMode, 'OnPlayerReconnect'), self)
+
+  ListenToGameEvent("dota_player_selected_custom_team", Dynamic_Wrap(GameMode, 'OnPlayerSelectedCustomTeam'), self)
+  
+  --ListenToGameEvent("dota_tutorial_shop_toggled", Dynamic_Wrap(GameMode, 'OnShopToggled'), self)
+
+  --ListenToGameEvent('player_spawn', Dynamic_Wrap(GameMode, 'OnNPCSpawned'), self)
+  --ListenToGameEvent('dota_unit_event', Dynamic_Wrap(GameMode, 'OnDotaUnitEvent'), self)
+  --ListenToGameEvent('nommed_tree', Dynamic_Wrap(GameMode, 'OnPlayerAteTree'), self)
+  --ListenToGameEvent('player_completed_game', Dynamic_Wrap(GameMode, 'OnPlayerCompletedGame'), self)
+  --ListenToGameEvent('dota_match_done', Dynamic_Wrap(GameMode, 'OnDotaMatchDone'), self)
+  --ListenToGameEvent('dota_combatlog', Dynamic_Wrap(GameMode, 'OnCombatLogEvent'), self)
+  --ListenToGameEvent('dota_player_killed', Dynamic_Wrap(GameMode, 'OnPlayerKilled'), self)
+  --ListenToGameEvent('player_team', Dynamic_Wrap(GameMode, 'OnPlayerTeam'), self)
+
+  --[[This block is only used for testing events handling in the event that Valve adds more in the future
+  Convars:RegisterCommand('events_test', function()
+      GameMode:StartEventTest()
+    end, "events test", 0)]]
+
+  --Convars:RegisterConvar('barebones_spew', tostring(spew), 'Set to 1 to start spewing barebones debug info.  Set to 0 to disable.', 0)
+
+  -- Change random seed
+  local timeTxt = string.gsub(string.gsub(GetSystemTime(), ':', ''), '0','')
+  math.randomseed(tonumber(timeTxt))
+
+  -- Initialized tables for tracking state
+  self.bSeenWaitForPlayers = false
+  self.vUserIds = {}
+
+end
+
+mode = nil
+
+-- This function is called as the first player loads and sets up the GameMode parameters
+function GameMode:_CaptureGameMode()
+  if mode == nil then
+    -- Set GameMode parameters
+    mode = GameRules:GetGameModeEntity()        
+    mode:SetRecommendedItemsDisabled( RECOMMENDED_BUILDS_DISABLED )
+    mode:SetCameraDistanceOverride( CAMERA_DISTANCE_OVERRIDE )
+    mode:SetCustomBuybackCostEnabled( CUSTOM_BUYBACK_COST_ENABLED )
+    mode:SetCustomBuybackCooldownEnabled( CUSTOM_BUYBACK_COOLDOWN_ENABLED )
+    mode:SetBuybackEnabled( BUYBACK_ENABLED )
+    mode:SetTopBarTeamValuesOverride ( USE_CUSTOM_TOP_BAR_VALUES )
+    mode:SetTopBarTeamValuesVisible( TOP_BAR_VISIBLE )
+    mode:SetUseCustomHeroLevels ( USE_CUSTOM_HERO_LEVELS )
+    mode:SetCustomHeroMaxLevel ( MAX_LEVEL )
+    mode:SetCustomXPRequiredToReachNextLevel( XP_PER_LEVEL_TABLE )
+
+    mode:SetBotThinkingEnabled( USE_STANDARD_DOTA_BOT_THINKING )
+    mode:SetTowerBackdoorProtectionEnabled( ENABLE_TOWER_BACKDOOR_PROTECTION )
+
+    mode:SetFogOfWarDisabled(DISABLE_FOG_OF_WAR_ENTIRELY)
+    mode:SetGoldSoundDisabled( DISABLE_GOLD_SOUNDS )
+    mode:SetRemoveIllusionsOnDeath( REMOVE_ILLUSIONS_ON_DEATH )
+
+    mode:SetAlwaysShowPlayerInventory( SHOW_ONLY_PLAYER_INVENTORY )
+    mode:SetAnnouncerDisabled( DISABLE_ANNOUNCER )
+    if FORCE_PICKED_HERO ~= nil then
+      mode:SetCustomGameForceHero( FORCE_PICKED_HERO )
+    end
+    mode:SetFixedRespawnTime( FIXED_RESPAWN_TIME ) 
+    mode:SetFountainConstantManaRegen( FOUNTAIN_CONSTANT_MANA_REGEN )
+    mode:SetFountainPercentageHealthRegen( FOUNTAIN_PERCENTAGE_HEALTH_REGEN )
+    mode:SetFountainPercentageManaRegen( FOUNTAIN_PERCENTAGE_MANA_REGEN )
+    mode:SetLoseGoldOnDeath( LOSE_GOLD_ON_DEATH )
+    mode:SetMaximumAttackSpeed( MAXIMUM_ATTACK_SPEED )
+    mode:SetMinimumAttackSpeed( MINIMUM_ATTACK_SPEED )
+    mode:SetStashPurchasingDisabled ( DISABLE_STASH_PURCHASING )
+
+    for rune, spawn in pairs(ENABLED_RUNES) do
+      mode:SetRuneEnabled(rune, spawn)
+    end
+
+    mode:SetUnseenFogOfWarEnabled(USE_UNSEEN_FOG_OF_WAR)
+
+    self:OnFirstPlayerLoaded()
+  end 
+end
+
+
